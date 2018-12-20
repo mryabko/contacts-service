9548d67e2ea331528e7852ca304a222724df0014
mryabko <maxryabko@gmail.com>
2018-12-20 12:39:04 -0800

    change port env

--- app.js
+++ app.js
@@ -113,6 +113,6 @@ function validateContact(contact) {
 
 // PORT
 
-const port = process.env.PORT || 3000;
+const port = process.env.port || process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
